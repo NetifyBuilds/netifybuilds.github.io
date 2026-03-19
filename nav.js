a11e1b249e79d3c295fc97821c17b5a58d6cdc02
NetifyBuilds <andrew@netifybuilds.com>
2026-03-19 09:16:34 -0400

    Create nav.js

---
+++ nav.js
@@ -0,0 +1,17 @@
+document.getElementById('nav-placeholder').innerHTML = `
+  <nav>
+    <a href="index.html" class="nav-logo">
+      <img src="favicon.png" class="nav-logo-icon" alt="Netify logo" />
+      Netify<span>.</span>
+    </a>
+    <ul class="nav-links">
+      <li><a href="index.html">Home</a></li>
+      <li><a href="index.html#how">How It Works</a></li>
+      <li><a href="index.html#services">Services</a></li>
+      <li><a href="index.html#pricing">Pricing</a></li>
+      <li><a href="about.html">About</a></li>
+      <li><a href="contact.html">Contact</a></li>
+      <li><a href="https://calendly.com/admin-netifybuilds/30min" target="_blank" class="nav-cta">Book a Call</a></li>
+    </ul>
+  </nav>
+`;
